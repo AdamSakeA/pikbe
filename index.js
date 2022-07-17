f5bf4c308f24a9e3deb8803a77e4840db910ea73
AdamSakeA <adamsake8@gmail.com>
2022-07-17 17:22:34 +0700

    test

--- index.js
+++ index.js
@@ -60,5 +60,7 @@ app.use(loginRoutes);
 app.use(buyingRoutes);
 app.use(adminRoutes);
 
+app.set('port', process.env.PORT || 5000);
+
 // app.listen(5000, ()=> console.log(`Server running on port 5000..`));
 app.listen(process.env.PORT || 5000, ()=> console.log(`Server running on port ${port}..`));
