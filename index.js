0f2010d8003e9d81c176dca332b1f7656c4fd7cb
AdamSakeA <adamsake8@gmail.com>
2022-07-18 16:33:15 +0700

    delete cors function

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ db.once('open', () => console.log("database connected.."));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser())
-// app.use(cors({ credentials: true, origin: "http://localhost:3000"}));
+app.use(cors());
 app.use(express.json());
 // app.use(userRoutes);
 console.log('productroutes: ', productRoutes);
